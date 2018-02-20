a8379eebc8680b00c2a127ec48d596db5c586f71
Eavan Pattie <e.pattie@warwick.ac.uk>
2018-02-20 16:43:51 +0000

    add single timeframe to node interface

--- analysis/node_interface.py
+++ analysis/node_interface.py
@@ -12,10 +12,18 @@ import frame_reader
 
 if __name__ == "__main__":
     input_args = json.loads(sys.stdin.readline())
+    reader = frame_reader.Stock_Reader()
 
     if input_args["request_type"] == "get_current_attribute":
-        reader = frame_reader.Stock_Reader()
         result = reader.get_current_attribute(
                 input_args["ticker"],
                 input_args["attribute"])
+        print(result)
+
+    if input_args["request_type"] == "get_attribute":
+        frame_name = reader.get_closest_frame(input_args["start_time"])
+        result = reader.get_attribute(
+                input_args["ticker"],
+                input_args["attribute"],
+                frame_name)
         print(result)
