90788159cab33e60bf2d5de256c8cfd3521b5bd5
Aaron Baw <aaronbaw@gmail.com>
2018-02-20 18:00:38 +0000

    Change symbol resolution API to favour LSE symbol codes & remove trails.

--- modules/resolve/lookupSymbol.js
+++ modules/resolve/lookupSymbol.js
@@ -7,9 +7,9 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const getQueryURL = (companyName) =>
-  `http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=` +
+  `http://autoc.finance.yahoo.com/autoc?query=` +
   `${companyName}` +
-  `&region=1&lang=en&callback=YAHOO.Finance.SymbolSuggest.ssCallback`;
+  `&lang=en-GB&region=UK`;
 
 
 // Define module entry point.
@@ -17,21 +17,30 @@ module.exports = (name, cb) => {
 
   request.get(getQueryURL(name), (err, res, body) => {
 
-    // Extract the JSON portion from the string.
-    // Just matches for the first and last curly brace to ignore the weird text at the beginning.
-    var coreResult =
-      body.slice(body.indexOf('{'), body.length - body.split('').reverse().join('').indexOf('}'));
+    // // Extract the JSON portion from the string.
+    // // Just matches for the first and last curly brace to ignore the weird text at the beginning.
+    // var coreResult =
+    //   body.slice(body.indexOf('{'), body.length - body.split('').reverse().join('').indexOf('}'));
 
-    body = JSON.parse(coreResult);
+    body = JSON.parse(body);
 
     // If there are no results, indicate query failure by returning a null.
     if (!body.ResultSet.Result || body.ResultSet.Result.length === 0)
       return cb(null);
 
-    cb({
-      symbol: body.ResultSet.Result[0].symbol,
-      name: body.ResultSet.Result[0].name
-    });
+    // Get first symbol & name from LSE, and strip trailing .L.
+    var output = null;
+
+    for (var item of body.ResultSet.Result){
+      if (item && item.exch === 'LSE') {
+        output = {
+          name: item.name,
+          symbol: item.symbol.replace(`.L`, ``)
+        }
+      }
+    }
+
+    return cb(output);
 
   });
 
