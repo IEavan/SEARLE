75a9480691d7197e38e374bc25bad91042c0bec3
Eavan Pattie <e.pattie@warwick.ac.uk>
2018-02-28 19:18:45 +0000

    started intent predictor

--- modules/analysis/ai.py
+++ modules/analysis/ai.py
@@ -6,15 +6,20 @@ and providing additional context to specific requests"""
 # Imports
 import os
 import json
+import pickle
+from itertools import product
+
+# Constants
+MARKOV_TUPLE_SIZE = 2
 
 class User_Log_Writer():
-    def __init__(self, log_path="./data/log/", hooks=[]):
+    def __init__(self, log_dir="./data/log/", hooks=[]):
         """ Records path to user data files and allows for
         hooks to be registered and attached to the
         append_request functions """
 
-        self.log_path = log_path
-        self.log_file = os.path.join(self.log_path, "user_log.json")
+        self.log_dir = log_dir
+        self.log_file = os.path.join(self.log_dir, "user_log.json")
         self.hooks = hooks
 
     def append_request(self, request):
@@ -33,10 +38,10 @@ class User_Log_Writer():
             hook(self.log_file)
 
 class User_Log_Reader():
-    def __init__(self, log_path="./data/log/"):
+    def __init__(self, log_dir="./data/log/"):
         """ Records path to user log files for later use """
-        self.log_path = log_path
-        self.log_file = os.path.join(self.log_path, "user_log.json")
+        self.log_dir = log_dir
+        self.log_file = os.path.join(self.log_dir, "user_log.json")
 
     def __getitem__(self, index):
         """ Extract the request corresponding to the given index
@@ -48,3 +53,17 @@ class User_Log_Reader():
             requests = log_dict["requests"]
 
         return requests[index]
+
+class Intent_Predictor():
+    def __init__(self, data_path="./data/log"):
+        """ Load the graph structure from the data path """
+        self.data_file    = os.path.join(data_path, "graph.txt")
+        self.intents_file = os.path.join(data_file, "intent_list.txt")
+
+        if os.path.isfile(self.data_file):
+            self.graph = pickle.load(self.data_file)
+        else:
+            self.graph = {}
+            with open(self.intents_file, 'r') as intents:
+                lines = intents.readlines()
+                for i in range(MARKOV_TUPLE_SIZE - 1):
